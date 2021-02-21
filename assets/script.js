a17cfe115d5e757fb1ff7df81529597c5bb584ba
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-02-21 15:02:26 -0500

    got units to show in fahrenheit

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,12 @@
-// Got openweathermap.org API key
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={5c1ecc9134e7903eacaef455960bdc82}
+let position= {
+        lat: -45 ,
+        lon: 112
+};
+fetch("https://api.openweathermap.org/data/2.5/weather?q=sicklerville&appid=5c1ecc9134e7903eacaef455960bdc82&units=imperial").then(response => response.json()).then(data => {
+        console.log(data);
+ });
+
+// fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,daily,alets&appid=5c1ecc9134e7903eacaef455960bdc82", {
+// }).then(response => response.json()).then(data => {
+//     console.log(data);
+// })
