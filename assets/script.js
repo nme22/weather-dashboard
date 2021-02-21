32feeb6aa685ef4e3d2d3c739b601303013db6bf
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-02-21 18:52:50 -0500

    set up search form

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,23 @@
+/* Pseudo Code
 
+When someone uses the search button, read the city name inserted by the user
+
+Using that same name, send a request from the weather api
+ From the response, get current conditions
+ using that same name, send another request to get 5-day forecast from weather api
+
+ Parse the response to display forecast for the next 5 days.
+
+ Save the user's search and display them in the search form
+
+
+
+
+
+
+
+
+*/
 function setupLocation(){
 fetch("https://api.openweathermap.org/data/2.5/weather?q=sicklerville&appid=5c1ecc9134e7903eacaef455960bdc82&units=imperial")
 .then(response => response.json()).then(data => {
@@ -10,12 +29,3 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=sicklerville&appid=5c1e
 function getData(cityCoord) {
 
 }
-
-
-setupLocation();
-// fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,daily,alets&appid=5c1ecc9134e7903eacaef455960bdc82", {
-// }).then(response => response.json()).then(data => {
-//     console.log(data);
-// })
-
-// 
